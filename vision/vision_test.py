354c04e5ace1d7745d76444ed2f98f47e314cd9f
Apre <herculanodavi@gmail.com>
2017-06-13 04:27:42 -0300

    visao estavel

--- vision/vision_test.py
+++ vision/vision_test.py
@@ -1,13 +1,16 @@
 import vision
 import camera
+import cv2
 
 vis = vision.Vision()
-#cam = camera.Camera()
+cam = camera.Camera()
 
 while(1):
-	img = cam.getFrame()
-	# lines are green
-	lines = vis.detect_lines(img)
+    img = cam.getFrame()
+    cv2.imshow('img', img)
+    cv2.waitKey(1)
+    # lines are green
+    lines = vis.detect_lines(img)
 
 	# car is blue
 	#car_pos = vis.detect_car(img)
