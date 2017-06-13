976804f95560be589f553bb394b468c0d1ccdc83
tessio <tessio.perotti@gmail.com>
2017-06-13 03:54:56 -0300

    Só falta o decisionMaking o/

--- run_bot.py
+++ run_bot.py
@@ -8,10 +8,17 @@ if __name__ == "__main__":
     model = Modeling()
     control = LineFollower()
     communication = SendData()
+    frente = 1
+    tras = -1
     model.update()
 
     # Aqui vem o decision_making
-
     desired_position = Pose2D()
     line_ref = Vector2()
     control.SetControlData(model.car(), desired_position, line_ref)
+    dutyMotor1 = control.DesiredVelocity()
+    dutyMotor2 = control.DesiredVelocity()
+    servoAngle = control.DesiredAngle()
+    direcao1 = frente
+    direcao2 = frente
+    communication.Send(direcao1, direcao2, dutyMotor1, dutyMotor2, servoAngle)
