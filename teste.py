e6bfa2a66cfb08ec69a33093c44d1b4ea92471b1
tessio <tessio.perotti@gmail.com>
2017-06-13 01:24:43 -0300

    Arquivos de teste do motor e servo

---
+++ teste.py
@@ -0,0 +1,56 @@
+# Programa de controle dos motores e do servo
+# Importando bibliotecas
+import RPi.GPIO as IO
+import time
+
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+
+# Estabelecendo pinos de comunicacao
+
+IO.setup(19,IO.OUT)
+IO.setup(22,IO.OUT)
+IO.setup(23,IO.OUT)
+
+# Definindo modo de operacao dos pinos
+
+servo = IO.PWM(19,50) # Pino 19, frequencia 50 Hz
+servo.start(10) # Duty cicle de inicio: 10%
+
+motor1 = IO.PWM(22,100) # Pino 22, frequencia 100 Hz
+motor1.start(15) # Duty cicle de inicio: 15%
+
+motor2 = IO.PWM(23,100) # Pino 23, frequencia 100 Hz
+motor2.start(15) # Duty cicle de inicio: 15%
+
+# Definindo constantes
+
+frente = 1
+
+tras = -1
+
+while True:
+	
+	for t in range (100):
+		# Testando limites de excussao dos motores e do servo
+		direcao = frente
+		velocidade = 15 + 1.0*direcao*5*t/100
+		angulo = 6.5 + 1.0*3*t/100
+		servo.ChangeDutyCycle(angulo)
+		motor1.ChangeDutyCycle(velocidade)
+		motor2.ChangeDutyCycle(velocidade)
+		print angulo
+		print t
+		time.sleep(0.1)
+
+	for t in range (100):
+		# Na diracao reversa
+		direcao = tras
+		velocidade = 15 + 1.0*direcao*5*t/100
+		angulo = 6.5 - 1.0*3*(100-t)/100
+		servo.ChangeDutyCycle(angulo)
+		motor1.ChangeDutyCycle(velocidade)
+		motor2.ChangeDutyCycle(velocidade)
+		print angulo
+		print t
+		time.sleep(0.1)
