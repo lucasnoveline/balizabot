1d3f0c23a4fa5b0cdd0f2587606f136a1b243001
lucasnoveline <lucasnoveline@gmail.com>
2017-06-13 15:48:15 -0300

    Create camera.py

--- vision/camera.py
+++ vision/camera.py
@@ -1,3 +1,7 @@
+# Código de inicialização da câmera. Caso a variável POKEMON == True
+# a câmera inicia com uma imagem externa ('pokemon.jpg'). Caso contrário
+# inicia a captura de imagens da câmera.
+
 POKEMON = False;
 
 #if not POKEMON:
@@ -8,6 +12,8 @@ import cv2
 class Camera():
     def __init__(self):
         if not POKEMON:
+            # Caso não POKEMON == False, inicia a captura da câmera
+            # com a biblioteca cv2.
             self.cap = cv2.VideoCapture(1)
             #self.cap.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 0.3)
             #self.cap.set(cv2.cv.CV_CAP_PROP_SATURATION, 0.5)
@@ -16,11 +22,14 @@ class Camera():
 
 
     def getFrame(self):
+        # Método getFrame
         if POKEMON:
+            # Caso POKEMON == True, abre 'pokemon.jpg'
             return cv2.imread('pokemon.jpg')
         else:
+            # Caso POKEMON == False, lê imagem da câmera.
+            ret, img = self.cap.read()
+            return img
             # grab an image from the camera
             # with picamera.array.PiRGBArray(self.camera) as stream:
             # return stream.array
-            ret, img = self.cap.read()
-            return img
